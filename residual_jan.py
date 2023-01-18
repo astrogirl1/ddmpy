ed8ad16cb4c4a394d2c1305bc06395a06b971704
Meera <meerand060@gmail.com>
2023-01-18 19:30:49 +1030

    plot size things mischa

--- residual_jan.py
+++ residual_jan.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import itertools
+plt.rcParams.update({"figure.autolayout":True})
 
 ## NOT USED####
 # files = ['/home/a1705053/Desktop/merged/merged_codes_may22/output23/lcdm_hyrec_cl.dat','/home/a1705053/Desktop/merged/merged_codes_may22/output23/test_g80_eps1e-50_cl.dat', '/home/a1705053/Desktop/merged/merged_codes_may22/output23/test_g80_eps1e-25_cl.dat', '/home/a1705053/Desktop/merged/merged_codes_may22/output23/test_g80_eps1e-10_cl.dat','/home/a1705053/Desktop/merged/merged_codes_may22/output23/test_g80_eps1e-5_cl.dat', '/home/a1705053/Desktop/merged/merged_codes_may22/output23/test_g80_eps1e-3_cl.dat', '/home/a1705053/Desktop/merged/merged_codes_may22/output23/test_g80_eps1e-2_cl.dat', '/home/a1705053/Desktop/merged/merged_codes_may22/output23/test_g80_eps5e-2_cl.dat']
@@ -13,9 +14,9 @@ import itertools
 
 ### USED FOR REPRODUCING POULIN PAPER########
 
-files = ['/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_lcdm_cl.dat',"/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_g300_cl.dat", "/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_g100_cl.dat", "/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_g30eps0.1_cl.dat", "/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_g10eps0.1_cl.dat"]
+# files = ['/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_lcdm_cl.dat',"/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_g300_cl.dat", "/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_g100_cl.dat", "/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_g30eps0.1_cl.dat", "/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_g10eps0.1_cl.dat"]
 
-# files = ['/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_lcdm_cl.dat',"/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_g30eps0.001_cl.dat", "/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_g30eps0.01_cl.dat", "/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_g30eps0.1_cl.dat", "/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_g30eps0.5_cl.dat"]
+files = ['/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_lcdm_cl.dat',"/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_g30eps0.001_cl.dat", "/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_g30eps0.01_cl.dat", "/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_g30eps0.1_cl.dat", "/home/a1705053/Desktop/merged/merged_codes_may22/tempout/test_g30eps0.5_cl.dat"]
 
 data = []
 for data_file in files:
@@ -24,13 +25,14 @@ for data_file in files:
 # 'test_g0.02', 'test_g0.0002', 'test_g0.000002', "test_g0.000002_eps1e-50"]
 #roots = ['lcdm', "g^-1 = 300","g^-1=100","g^-1=60","g^-1=30","g^-1=15"]
 #roots = ['lcdm', 'g^-1 = 300','g^-1=100','g^-1=60','g^-1=30','g^-1=15']
-roots = ['lcdm', r'$\Gamma_{wdm}^{-1}$ = 300 Gyr', '$\Gamma_{wdm}^{-1}$ = 100 Gyr', '$\Gamma_{wdm}^{-1}$ = 30 Gyr', '$\Gamma_{wdm}^{-1}$ = 10 Gyr']
+# roots = [r'$\Lambda$CDM', r'$\Gamma_{wdm}^{-1}$ = 300 Gyr', '$\Gamma_{wdm}^{-1}$ = 100 Gyr', '$\Gamma_{wdm}^{-1}$ = 30 Gyr', '$\Gamma_{wdm}^{-1}$ = 10 Gyr']
 # roots = ['Hyrec', 'Recfast']
-# roots = [r'$\Lambda$CDM', r'$\epsilon$ = 0.001 ', r'$\epsilon$ = 0.01 ', r'$\epsilon$ = 0.1 ', r'$\epsilon$ = 0.5 ']
+roots = [r'$\Lambda$CDM', r'$\epsilon$ = 0.001 ', r'$\epsilon$ = 0.01 ', r'$\epsilon$ = 0.1 ', r'$\epsilon$ = 0.5 ']
 
 #color = ['black', 'goldenrod','orange', 'darkorange', 'red', 'blueviolet'] 
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(12,6))
+
 #TT#
 index0, curve0 = 0, data[0]
 y_axis = [u'TT']
@@ -65,15 +67,17 @@ ax.semilogx(curve[:, 0], (curve[:, 1]/curve0[:, 1])-1, color='goldenrod')
 # ax.plot(curve[:, 0], (curve[:, 1]/curve0[:, 1])-1)
 
 ax.legend([root+': '+elem for (root, elem) in
-    itertools.product(roots, y_axis)], loc='lower left', fontsize=18)
+    itertools.product(roots, y_axis)], loc='lower left', fontsize=15)
 
-ax.set_xlabel('$\ell$', fontsize=20)
-ax.tick_params(labelsize=18)
+ax.set_xlabel('$\ell$', fontsize=22)
+ax.tick_params(labelsize=20)
 
-ax.set_ylabel(r'$\frac{C_\ell^{\mathrm{TT}}(\Lambda DDM)}{C_\ell^{\mathrm{TT}}(\Lambda CDM)}-1$',fontsize=22)
+ax.set_ylabel(r'$\frac{C_\ell^{\mathrm{TT}}(\Lambda DDM)}{C_\ell^{\mathrm{TT}}(\Lambda CDM)}-1$',fontsize=25)
 # ax.set_title(r"Residual plot with $\epsilon$ varied, $\Gamma_{wdm}^{-1}$= 30 Gyr ", loc='center')
 # ax.set_title(r"Residual plot for Hyrec vs Recfast", loc='center')
 ax.set(xlim=(2.5, 2700), ylim=(-0.17, 0.17))
+
+plt.savefig("epsvaried.pdf", format='pdf')
 plt.show()
 
 
