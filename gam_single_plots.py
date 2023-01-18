fdc7724f0ef57e51b28ddf1081d2d46e4d43e285
Meera <meerand060@gmail.com>
2023-01-18 19:43:42 +1030

    gam_single py file and chap5 final plot

--- gam_single_plots.py
+++ gam_single_plots.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
 import itertools
+plt.rcParams.update({"figure.autolayout":True})
+
 
 
 # files1 = ['/home/a1705053/Desktop/merged/merged_codes_may22/output/sec/lcdm_cl.dat',
@@ -19,16 +21,16 @@ import itertools
 
 
 # files1 = ['/home/a1705053/Desktop/merged/merged_codes_may22/january/lcdm_cl.dat','/home/a1705053/Desktop/merged/merged_codes_may22/january/eps_gam100_cl.dat', '/home/a1705053/Desktop/merged/merged_codes_may22/january/eps_gam30_cl.dat', '/home/a1705053/Desktop/merged/merged_codes_may22/january/eps_gam10_cl.dat']
-# files1= ['/home/a1705053/Desktop/merged/merged_codes_may22/january/lcdm_cl.dat', '/home/a1705053/Desktop/merged/merged_codes_may22/january/residual_2_cl.dat','/home/a1705053/Desktop/merged/merged_codes_may22/january/exoclass_2_cl.dat']
+files1= ['/home/a1705053/Desktop/merged/merged_codes_may22/january/lcdm_cl.dat', '/home/a1705053/Desktop/merged/merged_codes_may22/january/residual_2_cl.dat','/home/a1705053/Desktop/merged/merged_codes_may22/january/exoclass_2_cl.dat']
 # files2= ['/home/a1705053/Desktop/merged/merged_codes_may22/january/exoclass_lcdm_cl.dat','/home/a1705053/Desktop/merged/merged_codes_may22/january/exoclass_1_cl.dat']
 
-files2 = ['/home/a1705053/Desktop/merged/merged_codes_may22/output/sec/lcdm_cl.dat',
-'/home/a1705053/Desktop/merged/merged_codes_may22/output/wdm_eps/wdm_gam1.47_eps0.001_cl.dat' ,
-'/home/a1705053/Desktop/merged/merged_codes_may22/output/wdm_eps/wdm_gam1.47_eps0.003_cl.dat',
-'/home/a1705053/Desktop/merged/merged_codes_may22/output/wdm_eps/wdm_gam1.47_eps0.005_cl.dat' ,
-# '/home/a1705053/Desktop/merged/merged_codes_may22/output/wdm_eps/wdm_gam1.47_eps0.007_cl.dat',
-# '/home/a1705053/Desktop/merged/merged_codes_may22/output/wdm_eps/wdm_gam1.47_eps0.009_cl.dat',
-'/home/a1705053/Desktop/merged/merged_codes_may22/output/wdm_eps/wdm_gam1.47_eps0.01_cl.dat',]
+# files2 = ['/home/a1705053/Desktop/merged/merged_codes_may22/output/sec/lcdm_cl.dat',
+# '/home/a1705053/Desktop/merged/merged_codes_may22/output/wdm_eps/wdm_gam1.47_eps0.001_cl.dat' ,
+# '/home/a1705053/Desktop/merged/merged_codes_may22/output/wdm_eps/wdm_gam1.47_eps0.003_cl.dat',
+# '/home/a1705053/Desktop/merged/merged_codes_may22/output/wdm_eps/wdm_gam1.47_eps0.005_cl.dat' ,
+# # '/home/a1705053/Desktop/merged/merged_codes_may22/output/wdm_eps/wdm_gam1.47_eps0.007_cl.dat',
+# # '/home/a1705053/Desktop/merged/merged_codes_may22/output/wdm_eps/wdm_gam1.47_eps0.009_cl.dat',
+# '/home/a1705053/Desktop/merged/merged_codes_may22/output/wdm_eps/wdm_gam1.47_eps0.01_cl.dat',]
 # '/home/a1705053/Desktop/merged/merged_codes_may22/output/wdm_eps/wdm_gam1.47_eps0.015_cl.dat',
 # '/home/a1705053/Desktop/merged/merged_codes_may22/output/wdm_eps/wdm_gam1.47_eps0.02_cl.dat',
 # '/home/a1705053/Desktop/merged/merged_codes_may22/output/wdm_eps/wdm_gam1.47_eps0.025_cl.dat']
@@ -43,7 +45,7 @@ for data_file in files1:
 # "$\Gamma_{wdm}^{-1}$ = 55.27 Gyr", 
 # "$\Gamma_{wdm}^{-1}$ = 24.6 Gyr" ]
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(10,6))
 
 y_axis = [u'TT']
 tex_names = ['TT']
@@ -51,7 +53,7 @@ x_axis = 'l'
 ylim = []
 xlim = []
 
-color = ['goldenrod','blue', 'green','chocolate', 'orangered', 'orchid', 'orange', 'magenta','red']
+color = ['red','blue', 'green','chocolate', 'orangered', 'orchid', 'orange', 'magenta','red']
 
 
 index0, curve0 = 0, data1[0]
@@ -77,21 +79,21 @@ for idx, dat in enumerate(data1[1:]):
 # fig.legend([root for (root, elem) in
 #     itertools.product(roots, y_axis)], loc='best')
 
-ax.set_xlabel('$\ell$', fontsize=20)
+ax.set_xlabel('$\ell$', fontsize=21)
 ax.set_xlim(3,2700)
 
 # ax[0].annotate(r'$\Gamma_{wdm}^{-1} = 55 Gyr$', xy=(10.5, 8e-10))
-ax.tick_params(labelsize=18)
+ax.tick_params(labelsize=21)
 
-plt.ylabel(r'$\frac{C_\ell^\mathrm{TT}(\Lambda\mathrm{DDM})}{C_\ell^\mathrm{TT}(\Lambda \mathrm{CDM})}-1$',fontsize=20)
+plt.ylabel(r'$\frac{C_\ell^\mathrm{TT}(\Lambda\mathrm{DDM})}{C_\ell^\mathrm{TT}(\Lambda \mathrm{CDM})}-1$',fontsize=23)
 # plt.ylabel(r'$C_\ell^\mathrm{TT}$',fontsize=15)
 # plt.title("pe varied, $\Gamma_{exo}$ = 1e-2 km/s/Mpc ", loc='center')
 # plt.title("$\Gamma$ varied , $\epsilon=0.0067$ ", loc='center')
-plt.legend([r'$\Lambda$CDM', r'$\epsilon=0.001$',r'$\epsilon=0.01$', r'$\epsilon=0.1$', r'$\epsilon=0.5$'])
-# plt.legend([r'$\Lambda$CDM', r'Modified CLASS(2023) $(10^{22}s)$',r'ExoCLASS $(10^{22}s)$'], fontsize=18, loc='upper left')
+# plt.legend([r'$\Lambda$CDM', r'$\epsilon=0.001$',r'$\epsilon=0.01$', r'$\epsilon=0.1$', r'$\epsilon=0.5$'])
+plt.legend([r'$\Lambda$CDM', r'Modified CLASS(2023) $(10^{22}s)$',r'ExoCLASS $(10^{22}s)$'], fontsize=19, loc='upper left', framealpha=0.1)
 # plt.title(r'Exoclass vs Merged Code', loc='center')
 # plt.savefig("residual_2_gamwdm1e-10kmsMpc.pdf", format="pdf")
-plt.savefig("chap4epsilon.pdf", format="pdf")
+plt.savefig("chap5merged_test.pdf", format="pdf")
 
 
 plt.show()
